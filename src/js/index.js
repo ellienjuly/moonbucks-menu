05d6e8b34bb65ff2dc6e5d339e73b5e6a6a921d4
Ellie Nam <ellienjuly@gmail.com>
2021-11-09 10:24:48 -0500

    Add a menu under the input field using insertAdjacentHTML

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const $ = selector => document.querySelector(selector);
+
+function App() {
+  $('#espresso-menu-form').addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+  //get menu names
+  $('#espresso-menu-name').addEventListener("keypress", (e) => {
+      if (e.key === 'Enter') {
+        const espressoMenuName = $('#espresso-menu-name').value
+        const menuItemTemplate = espressoMenuName => {
+        return `
+        <li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
+              Edit
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
+              Delete
+            </button>
+          </li>`;
+        };
+        $('#espresso-menu-list').insertAdjacentHTML(
+          'beforeend',
+          menuItemTemplate(espressoMenuName)
+        );
+      };
+    });
+}
+
+App();
